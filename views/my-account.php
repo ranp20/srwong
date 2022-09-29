582ca1363e584769875c9e65e2b46b260b166c5a
ranp20 <polancoranp2019@gmail.com>
2022-09-29 09:33:46 -0500

    repair profile user

--- views/my-account.php
+++ views/my-account.php
@@ -33,147 +33,111 @@ session_start();
       <div class="row">
         <div class="ml-auto mr-auto col-lg-9">
           <div class="checkout-wrapper">
-              <div id="faq" class="panel-group">
-                  <div class="panel panel-default">
-                      <div class="panel-heading">
-                          <h5 class="panel-title"><span>1</span> <a data-toggle="collapse" data-parent="#faq" href="#my-account-1">Edit your account information </a></h5>
-                      </div>
-                      <div id="my-account-1" class="panel-collapse collapse show">
-                          <div class="panel-body">
-                              <div class="billing-information-wrapper">
-                                  <div class="account-info-wrapper">
-                                      <h4>My Account Information</h4>
-                                      <h5>Your Personal Details</h5>
-                                  </div>
-                                  <div class="row">
-                                      <div class="col-lg-6 col-md-6">
-                                          <div class="billing-info">
-                                              <label>First Name</label>
-                                              <input type="text">
-                                          </div>
-                                      </div>
-                                      <div class="col-lg-6 col-md-6">
-                                          <div class="billing-info">
-                                              <label>Last Name</label>
-                                              <input type="text">
-                                          </div>
-                                      </div>
-                                      <div class="col-lg-12 col-md-12">
-                                          <div class="billing-info">
-                                              <label>Email Address</label>
-                                              <input type="email">
-                                          </div>
-                                      </div>
-                                      <div class="col-lg-6 col-md-6">
-                                          <div class="billing-info">
-                                              <label>Telephone</label>
-                                              <input type="text">
-                                          </div>
-                                      </div>
-                                      <div class="col-lg-6 col-md-6">
-                                          <div class="billing-info">
-                                              <label>Fax</label>
-                                              <input type="text">
-                                          </div>
-                                      </div>
-                                  </div>
-                                  <div class="billing-back-btn">
-                                      <div class="billing-back">
-                                          <a href="#"><i class="ion-arrow-up-c"></i> back</a>
-                                      </div>
-                                      <div class="billing-btn">
-                                          <button type="submit">Continue</button>
-                                      </div>
-                                  </div>
-                              </div>
+            <div id="faq" class="panel-group">
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h5 class="panel-title"><span>1</span> <a data-toggle="collapse" data-parent="#faq" href="#my-account-1">Edite la información de su cuenta </a></h5>
+                </div>
+                <div id="my-account-1" class="panel-collapse collapse show">
+                  <div class="panel-body">
+                    <form action="" method="POST" id="clx-frm_usval-info">                              
+                      <div class="profile-information-wrapper">
+                        <div class="account-info-wrapper">
+                          <h4><strong>DATOS PERSONALES</strong></h4>
+                        </div>
+                        <div class="row">
+                          <div class="col-lg-6 col-md-6">
+                            <div class="profile-info">
+                              <label>Nombres</label>
+                              <input type="text" placeholder="" required>
+                            </div>
                           </div>
-                      </div>
-                  </div>
-                  <div class="panel panel-default">
-                      <div class="panel-heading">
-                          <h5 class="panel-title"><span>2</span> <a data-toggle="collapse" data-parent="#faq" href="#my-account-2">Change your password </a></h5>
-                      </div>
-                      <div id="my-account-2" class="panel-collapse collapse">
-                          <div class="panel-body">
-                              <div class="billing-information-wrapper">
-                                  <div class="account-info-wrapper">
-                                      <h4>Change Password</h4>
-                                      <h5>Your Password</h5>
-                                  </div>
-                                  <div class="row">
-                                      <div class="col-lg-12 col-md-12">
-                                          <div class="billing-info">
-                                              <label>Password</label>
-                                              <input type="password">
-                                          </div>
-                                      </div>
-                                      <div class="col-lg-12 col-md-12">
-                                          <div class="billing-info">
-                                              <label>Password Confirm</label>
-                                              <input type="password">
-                                          </div>
-                                      </div>
-                                  </div>
-                                  <div class="billing-back-btn">
-                                      <div class="billing-back">
-                                          <a href="#"><i class="ion-arrow-up-c"></i> back</a>
-                                      </div>
-                                      <div class="billing-btn">
-                                          <button type="submit">Continue</button>
-                                      </div>
-                                  </div>
-                              </div>
+                          <div class="col-lg-6 col-md-6">
+                            <div class="profile-info">
+                              <label>Apellidos</label>
+                              <input type="text" placeholder="" required>
+                            </div>
                           </div>
-                      </div>
-                  </div>
-                  <div class="panel panel-default">
-                      <div class="panel-heading">
-                          <h5 class="panel-title"><span>3</span> <a data-toggle="collapse" data-parent="#faq" href="#my-account-3">Modify your address book entries   </a></h5>
-                      </div>
-                      <div id="my-account-3" class="panel-collapse collapse">
-                          <div class="panel-body">
-                              <div class="billing-information-wrapper">
-                                  <div class="account-info-wrapper">
-                                      <h4>Address Book Entries</h4>
-                                  </div>
-                                  <div class="entries-wrapper">
-                                      <div class="row">
-                                          <div class="col-lg-6 col-md-6 d-flex align-items-center justify-content-center">
-                                              <div class="entries-info text-center">
-                                                  <p>Farhana hayder (shuvo) </p>
-                                                  <p>hastech </p>
-                                                  <p> Road#1 , Block#c </p>
-                                                  <p> Rampura. </p>
-                                                  <p>Dhaka </p>
-                                                  <p>Bangladesh </p>
-                                              </div>
-                                          </div>
-                                          <div class="col-lg-6 col-md-6 d-flex align-items-center justify-content-center">
-                                              <div class="entries-edit-delete text-center">
-                                                  <a class="edit" href="#">Edit</a>
-                                                  <a href="#">Delete</a>
-                                              </div>
-                                          </div>
-                                      </div>
-                                  </div>
-                                  <div class="billing-back-btn">
-                                      <div class="billing-back">
-                                          <a href="#"><i class="ion-arrow-up-c"></i> back</a>
-                                      </div>
-                                      <div class="billing-btn">
-                                          <button type="submit">Continue</button>
-                                      </div>
-                                  </div>
-                              </div>
+                          <div class="col-lg-6 col-md-6">
+                            <div class="profile-info">
+                              <label>Correo Electrónico</label>
+                              <input type="email" placeholder="" required>
+                            </div>
                           </div>
+                          <div class="col-lg-6 col-md-6">
+                            <div class="profile-info">
+                              <label>Teléfono</label>
+                              <input type="text" placeholder="" required>
+                            </div>
+                          </div>
+                          <div class="col-lg-6 col-md-6">
+                            <div class="profile-info">
+                              <label>Tipo de Documento</label>
+                              <select class="form-control one-hidden" aria-required="true" name="prof_usval-location" id="prof_usval-location" required>
+                                <option value="">Seleccione una opción</option>
+                                <option value="1" required>DNI</option>
+                                <option value="2" required>Carnet de extranjería</option>
+                                <option value="3" required>Pasaporte</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div class="col-lg-6 col-md-6">
+                            <div class="profile-info">
+                              <label>N° de Documento</label>
+                              <input type="text" placeholder="" required>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="profile-back-btn">
+                          <div class="profile-back">
+                            <a href="#"><i class="ion-arrow-up-c"></i> back</a>
+                          </div>
+                          <div class="profile-btn">
+                            <button type="submit">Guardar</button>
+                          </div>
+                        </div>
                       </div>
+                    </form>
                   </div>
-                  <div class="panel panel-default">
-                      <div class="panel-heading">
-                          <h5 class="panel-title"><span>4</span> <a href="wishlist">Modify your wish list   </a></h5>
-                      </div>
-                  </div>
+                </div>
               </div>
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h5 class="panel-title"><span>2</span> <a data-toggle="collapse" data-parent="#faq" href="#my-account-2">Mis Pedidos </a></h5>
+                </div>
+                <div id="my-account-2" class="panel-collapse collapse">
+                  <div class="panel-body">
+                    <div class="profile-information-wrapper">
+                      <div class="account-info-wrapper">
+                        <h4><strong>Cambiar contraseña</strong></h4>
+                      </div>
+                      <div class="row">
+                        <div class="col-lg-12 col-md-12">
+                          <div class="profile-info">
+                            <label>Contraseña</label>
+                            <input type="password">
+                          </div>
+                        </div>
+                        <div class="col-lg-12 col-md-12">
+                          <div class="profile-info">
+                            <label>Confirmar contraseña</label>
+                            <input type="password">
+                          </div>
+                        </div>
+                      </div>
+                      <div class="profile-back-btn">
+                        <div class="profile-back">
+                          <a href="#"><i class="ion-arrow-up-c"></i> back</a>
+                        </div>
+                        <div class="profile-btn">
+                          <button type="submit">Guardar</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
