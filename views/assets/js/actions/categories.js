386835106459a6850f08d930e866608923f2ba0d
ranp20 <polancoranp2019@gmail.com>
2022-10-03 23:28:59 -0500

    repair changes into styles for server 11

---
+++ views/assets/js/actions/categories.js
@@ -0,0 +1,146 @@
+// ------------ ENCRIPTAR DATOS DE INPUTS
+function encryptValuesIpts(valueipt){
+  let ciphertext = CryptoJS.AES.encrypt(valueipt, 'CML_KEYSYSTEM').toString();
+  return ciphertext;
+}
+// ------------ DESENCRIPTAR DATOS DE INPUTS
+function decryptValuesIpts(valueipt){
+  let bytes  = CryptoJS.AES.decrypt(valueipt, 'CML_KEYSYSTEM');
+  let originalText = bytes.toString(CryptoJS.enc.Utf8);
+  return originalText;
+}
+// ------------ COMPROBAR SI ES NUMÉRICO
+function isNumeric(variable){return !isNaN(parseInt(variable));}
+$(() => {
+	// ------------ ENCRIPTACIÓN DE INPUTS
+  var encrypt_sess_idcli = $("#u-s_regclient-sis").val(encryptValuesIpts($("#u-s_regclient-sis").val()));
+  // ------------ DESENCRIPTACIÓN DE INPUTS
+  var sess_idcli = decryptValuesIpts(encrypt_sess_idcli.val());
+	// ------------ LISTAR LOS PRODUCTOS EN EL CARRITO DE DICHO CLIENTE
+  listCartList();
+  // ------------ LISTAR EL CARRITO DE COMPRAS
+  function listCartList(){
+    if(isNumeric(sess_idcli) == true || isNumeric(sess_idcli) == "true"){
+      $("#c-listCartU").html("");
+      $.ajax({
+        url: "./controllers/prcss_cart-list-byIdTempCart.php",
+        method: "POST",
+        dataType: 'JSON',
+        contentType: 'application/x-www-form-urlencoded;charset=UTF-8',
+        data: { idcli : sess_idcli},
+        success : function(e){
+          if(e != "" && e != "[]"){
+            let tmpList = "";
+            tmpList += `<ul style='max-height: 335px;overflow-x: hidden;overflow-y: auto;'>`;
+            // ------------ SUMAR LOS SUBTOTALES DE TODOS LOS PRODUCTOS
+            let filtered = Object.entries(e);
+            // let totalpay = filtered.reduce(function(sum, v){
+            //   return sum + parseFloat(v.tmp_subtotal)
+            // }, 0);
+            let totalpay = 0;
+            $.each(e, function(i,v){
+              totalpay += parseFloat(v.tmp_subtotal);
+            });
+            // ------------ AGREGAR DOS CEROS AL FINAL DE CADA NÚMERO SIN UNO O DOS CEROS
+            var tpay_wzero = Number(totalpay);
+            var res = totalpay.toString().split(".");
+            if(res.length == 1 || (res[1].length < 3)) {
+              tpay_wzero = tpay_wzero.toFixed(2);
+            }
+            $("#c-totcart").html(`
+              <div class="header-icon-style">
+                <i class="icon-handbag icons"></i>
+                <span class="count-style">${filtered.length}</span>
+              </div>
+              <div class="cart-text">
+                <span class="digit">Mi Carrito</span>
+                <span class="cart-digit-bold">S/. ${tpay_wzero}</span>
+              </div>
+            `);
+            $.each(e, function(i,v){
+              let p_pathimg = "./admin/storage/app/public/product/"+v.p_photo;
+              let p_name = v.p_name;
+              let p_name_limit = (p_name.length >= 20) ? p_name.substring(20, 0) + "..." : p_name;
+              tmpList += `
+                <li class="single-shopping-cart">
+                  <div class="shopping-cart-img">
+                    <a href="./product-details/${v.id_product}">
+                      <img alt="" src="${p_pathimg}" alt="${p_name_limit}">
+                    </a>
+                  </div>
+                  <div class="shopping-cart-title">
+                    <h4><a href="./product-details/${v.id_product}">${p_name_limit} </a></h4>
+                    <h6>Cantidad: ${v.tmp_quantity}</h6>
+                    <span>S/. ${v.tmp_subtotal}</span>
+                  </div>
+                </li>
+              `;
+            });
+            tmpList += `</ul>`;
+            tmpList += `
+              <div class="shopping-cart-total">
+                <h4>Total : <span class="shop-total">S/. ${tpay_wzero}</span></h4>
+              </div>
+              <div class="shopping-cart-btn">
+                <a href="cart-page" id="lk_cart">Ver Carrito</a>
+                <!-- <a href="checkout" id="lk_checkout">checkout</a> -->
+              </div>
+            `;
+            $("#c-listCartU").html(tmpList);
+          }else{
+            $("#c-totcart").html(`
+              <div class="header-icon-style">
+                <i class="icon-handbag icons"></i>
+                <span class="count-style"> 0 </span>
+              </div>
+              <div class="cart-text">
+                <span class="digit">Mi Carrito</span>
+                <span class="cart-digit-bold">S/. 0.00</span>
+              </div>
+            `);
+            $("#c-listCartU").html(`
+              <ul>
+                <li class='single-shopping-cart'>
+                  <div class='shopping-cart-title' style='padding-bottom: 20px;'>
+                    <h4>No hay productos</h4>
+                  </div>
+                </li>
+              </ul>
+              <div class='shopping-cart-btn'>
+                <a href='./cart-page' id='logg-lk_cart-s'>Carrito</a>
+              </div>
+            `);
+          }
+        },
+        error : function(xhr, status){
+          console.log('Disculpe, existió un problema');
+        }
+      });
+    }else{
+      $("#c-totcart").html(`
+        <div class="header-icon-style">
+          <i class="icon-handbag icons"></i>
+          <span class="count-style"> 0 </span>
+        </div>
+        <div class="cart-text">
+          <span class="digit">Mi Carrito</span>
+          <span class="cart-digit-bold">S/. 0.00</span>
+        </div>
+      `);
+      $("#c-listCartU").html(`
+        <ul>
+          <li class='single-shopping-cart'>
+            <div class='shopping-cart-title' style='padding-bottom: 20px;'>
+              <h4>No hay productos</h4>
+            </div>
+          </li>
+        </ul>
+        <div class='shopping-cart-btn'>
+          <a href='./cart-page' id='logg-lk_cart-s'>Carrito</a>
+        </div>
+      `);
+    }
+  }
+  // ------------ IR HACIA LA PÁGINA - CART LIST (VALIDAR LA SESIÓN)
+  $(document).on("click","#logg-lk_cart-s",function(){window.location.href = "./";});
+});
--- views/categories.php
+++ views/categories.php
@@ -3,7 +3,7 @@
 (substr_count($_SERVER["HTTP_ACCEPT_ENCODING"], "gzip")) ? ob_start("ob_gzhandler") : ob_start();
 session_start();
 require_once '../model/categories.php';
-$catg = new Categories();
+$categories = new Categories();
 
 ?>
 <!DOCTYPE html>
@@ -49,7 +49,7 @@ $catg = new Categories();
             <div class="product-grid product-view pb-20">
               <div class="row">
                 <!-- LIST ALL PRODUCTS BY NAME CATEGORIES (INIT) -->
-                <?php echo $catg->getAllCategories();?>
+                <?php echo $categories->getAllCategories();?>
                 <!-- LIST ALL PRODUCTS BY NAME CATEGORIES (END) -->
               </div>
             </div>
@@ -58,112 +58,10 @@ $catg = new Categories();
       </div>
     </div>
   </div>
-  
-  
-  <div class="footer-area black-bg-2 pt-70">
-  <div class="footer-top-area pb-18">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-4 col-md-6 col-sm-6">
-
-
-          <div class="footer-about mb-40">
-            <div class="footer-logo">
-              <a href="index">
-                <img src="<?= $url;?>assets/img/logo/logo.png" alt="">
-              </a>
-            </div>
-            <p>Restaurante de venta de comida china.</p>  
-          </div>
-
-        </div>
-        <div class="col-lg-2 col-md-6 col-sm-6">
-          <div class="footer-widget mb-40">
-            <div class="footer-title mb-22">
-              <h4>CATEGORÍAS</h4>
-            </div>
-            <div class="footer-content">
-              <ul>
-                <?php
-                  $tmp_ctgshort = "";
-                  foreach($shortListCatg as $k => $v){
-                    $catg_name = mb_convert_case($v['name'], MB_CASE_TITLE, "UTF-8");
-                    $tmp_ctgshort .= "<li><a href='./category/{$v['id']}'>{$catg_name}</a></li>";
-                  }
-                  echo $tmp_ctgshort;
-                ?>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-2 col-md-6 col-sm-6">
-          <div class="footer-widget mb-40">
-            <div class="footer-title mb-22">
-              <h4>MENÚ</h4>
-            </div>
-            <div class="footer-content">
-              <ul>
-                <li><a href="my-account">Perfil</a></li>
-                <li><a href="about-us">Nosotros</a></li>
-                <li><a href="wishlist">Políticas de privacidad</a></li>
-                <li><a href="javascript:void(0);">Términos y condiciones</a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4 col-md-6 col-sm-6">
-          <div class="footer-widget mb-40">
-            <div class="footer-title mb-22">
-              <h4>INFORMACIÓN DE CONTACTO</h4>
-            </div>
-            <div class="footer-contact">
-              <ul>
-                <li>Av. Los Jardines este 173 Urb. Las flores 78 S.J.L</li>
-                <li>Teléfonos: (01) 612 8000 </li>
-                <li>Email: <a href="javascript:void(0);">ventas@srwong.pe</a></li>
-              </ul>
-            </div>
-            <div class="mt-35 footer-title mb-22">
-              <h4>Horarios</h4>
-            </div>
-            <div class="footer-time">
-              <ul>
-                <li>Abierto:  <span>8:00 AM</span> - Cerrado: <span>18:00 PM</span></li>
-                <li>Sábado y Domingo: <span>Abierto</span></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="footer-bottom-area border-top-4">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-6 col-md-6 col-sm-7">
-          <div class="copyright">
-            <p>Copyright © <a href="javascript:void(0);">SrWong.</a> Todos los derechos reservados.</p>
-          </div>
-        </div>
-        <div class="col-lg-6 col-md-6 col-sm-5">
-          <div class="footer-social">
-            <ul>
-              <li><a href="javascript:void(0);"><i class="ion-social-facebook"></i></a></li>
-              <li><a href="javascript:void(0);"><i class="ion-social-twitter"></i></a></li>
-              <li><a href="javascript:void(0);"><i class="ion-social-instagram-outline"></i></a></li>
-              <li><a href="javascript:void(0);"><i class="ion-social-googleplus-outline"></i></a></li>
-              <li><a href="javascript:void(0);"><i class="ion-social-rss"></i></a></li>
-              <li><a href="javascript:void(0);"><i class="ion-social-dribbble-outline"></i></a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
+  <?php require_once 'includes/inc_footer.php';?>
   <?php require_once 'includes/inc_mobile-tabs-links-footer.php';?>
 	<!-- all js here -->
   <script type="text/javascript" src="<?= $url;?>assets/js/main.js"></script>
-  <script type="text/javascript" src="<?= $url;?>assets/js/actions/category.js"></script>
+  <script type="text/javascript" src="<?= $url;?>assets/js/actions/categories.js"></script>
 </body>
 </html>
