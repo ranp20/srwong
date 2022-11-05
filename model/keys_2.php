fcf70f0c73f2c75d7c5c40ace68a7378c3f0ab89
ranp20 <polancoranp2019@gmail.com>
2022-11-05 12:08:09 -0600

    repair changes into admin

--- model/keys_2.php
+++ model/keys_2.php
@@ -6,19 +6,20 @@ require_once '../vendor/autoload.php';
 $fk_NameServerAPIREST = "https://api.micuentaweb.pe";
 $fk_Username = "58822376";
 
-/* ----- CREDENCIALES DE PRUEBA
+// ------ CREDENCIALES DE PRUEBA
+
 $fk_Password = "testpassword_94yzztM4sQQmn6ZcbvqE1y2J2EYstD6RBP32M0iq0xKtb";
 $fk_Token = "NTg4MjIzNzY6dGVzdHBhc3N3b3JkXzk0eXp6dE00c1FRbW42WmNidnFFMXkySjJFWXN0RDZSQlAzMk0waXEweEt0Yg==";
 $fk_Publickey = "58822376:testpublickey_7wC78NG1rvrnxfk0OdyveMujZYy8L43fLWKE7VUAB21zh";
 $fk_SHA_256 = "UCjCI3AM5rlmGBu3tbF1Jg6FDehXzyon6cZ8DB0ZbvD2s";
-*/
 
-// ----- CREDENCIALES DE PRODUCCIÓN*/
+// ------ CREDENCIALES DE PRODUCCIÓN
+/*
 $fk_Password = "prodpassword_1gv8XMvH7IzzwpWWzaPzEICbg5i3uPQILVNvM2XYsGU6T";
 $fk_Token = "NTg4MjIzNzY6dGVzdHBhc3N3b3JkXzk0eXp6dE00c1FRbW42WmNidnFFMXkySjJFWXN0RDZSQlAzMk0waXEweEt0Yg==";
 $fk_Publickey = "58822376:publickey_2zfN8vDx4xshCGb3wvOtCCGajSBbWh2UEAYtWXvs39mzk";
 $fk_SHA_256 = "kxYEMaxXXqW702hHW0xywkEb5H6lhm9Iwp32BHPHQYgqB";
-
+*/
 // CREDENCIALES REALES (INICIO)
 Lyra\Client::setDefaultUsername($fk_Username); // NOMBRE DE USUARIO
 Lyra\Client::setDefaultPassword($fk_Password); // PASSWORD
